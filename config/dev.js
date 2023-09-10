b0dd2fd1452265fbb294342a1d6e5af26d28fe7b
Numan Ahmed <mdnmnahmed@gmail.com>
2023-09-10 11:17:57 +0530

    backend .env files changes

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB_URI: "mongodb://localhost:27017/prac_db"
+    MONGODB_URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qzgcd.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
 }
--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB_URI: "process.env.MONGODB_URI"
+    MONGODB_URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qzgcd.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
 }
